bf37ca487f7245f0aa491b3562d1616d05b17dd4
Mike Jarrett <msjarrett@gmail.com>
2017-03-31 10:37:24 -0700

    line picking is working

--- python_data_vis/assignment4.py
+++ python_data_vis/assignment4.py
@@ -6,6 +6,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import pearsonr
+import matplotlib.patches as mpatches
 
 sns.set_style('white')
 
@@ -49,8 +50,8 @@ f,ax = plt.subplots(2,2)
 #         #     line[0].set_alpha(1)
 
 def make_timeseries(ax,df):
-    lines = df[-24:].plot(c='grey',alpha=0.15,ax=ax,legend=False,picker=2)
-    london_line = df[-24:]['London'].plot(c=sns.color_palette()[1],ax=ax,legend=False,picker=2)
+    lines = df[-24:].plot(c='grey',alpha=0.15,ax=ax,legend=False,picker=4)
+    london_line = df[-24:]['London'].plot(c=sns.color_palette()[1],ax=ax,legend=False,picker=0)
     return lines, london_line
 
 
@@ -61,7 +62,7 @@ rdf_lines, rdf_london_line = make_timeseries(ax[1][0],rdf)
 ax[0][0].set_title('Fractional Change in Housing Starts from 1992 for London and Other Canadian Cities')
 ax[1][0].set_title('Rental Vacancy Rate 1992-2015 for London and Other Canadian Cities')
 ax[0][0].set_ylabel('Change Housing Starts')
-ax[1][0].set_ylabel('Rental Vacancy Rate')
+ax[1][0].set_ylabel('Rental Vacancy Raimport matplotlib.patches as mpatcheste')
 
 
 sns.regplot(x=rdf[-24:]['London'],y=hdf[-24:]['London'],ax=ax[0][1])
@@ -72,10 +73,33 @@ sns.despine()
 plt.tight_layout()
 
 
+
+london_patch = mpatches.Patch(color=sns.color_palette()[1], label='London')
+ax[0][0].legend(handles=[london_patch])
+ax[1][0].legend(handles=[london_patch])
+
 def onpick(event):
-    #if event.artist not in dhdf_lines + rdf_lines: return True
-    event.artist.set_color('red')
+
+    if event.artist.get_color() == sns.color_palette()[2]:
+        event.artist.set_color('grey')
+        event.artist.set_alpha(0.15)
+        event.artist.axes.legend(handles=[london_patch])
+    else:
+        for line in ax[0][0].lines + ax[1][0].lines:
+            if line.get_label() != 'London':
+                line.set_color('grey')
+                line.set_alpha(0.15)
+
+        if event.artist.get_label() != 'London':
+            event.artist.set_color(sns.color_palette()[2])
+            event.artist.set_alpha(0.8)
+            pick_patch = mpatches.Patch(color=sns.color_palette()[2], label=event.artist.get_label())
+            event.artist.axes.legend(handles=[london_patch,pick_patch])
     f.canvas.draw_idle()
-    print(event.artist)
+
 f.canvas.mpl_connect('pick_event', onpick)
+
+
+
+
 plt.show()
